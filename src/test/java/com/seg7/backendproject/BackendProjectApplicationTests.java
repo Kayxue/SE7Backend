98ec1d4a9d314bd701a5366a799331383e0305de
Kayxue <s0990006@gmail.com>
2024-10-21 12:07:04 +0800

    2024/10/21 Init

---
+++ src/test/java/com/seg7/backendproject/BackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seg7.backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
