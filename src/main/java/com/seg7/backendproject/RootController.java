b95e7c2bfc16335d45771e9878e12f6aebca6e74
lemonade-00 <68552165+lemonade-00@users.noreply.github.com>
2024-10-21 16:54:38 +0800

    Update RootController.java

--- src/main/java/com/seg7/backendproject/RootController.java
+++ src/main/java/com/seg7/backendproject/RootController.java
@@ -1,12 +1,38 @@
 package com.seg7.backendproject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.seg7.backendproject.MyService;
+import com.seg7.backendproject.Accounts;
+import com.seg7.backendproject.AccountRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 public class RootController {
+
+    @Autowired
+	private MyService myService;
+    
     @GetMapping("/")
     public String root() {
         return "Hello World";
     }
+
+    @PostMapping
+	public ResponseEntity<Accounts> createProduct(@Valid @RequestBody AccountRequest request) {
+		Accounts accounts = myService.createProduct(request);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(accounts.getID())
+				.toUri();
+
+		return ResponseEntity.created(location).body(accounts);
+	}
 }
