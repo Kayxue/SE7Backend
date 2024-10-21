9a480face74999fe28f4bc99d00c85d17294579f
lemonade-00 <68552165+lemonade-00@users.noreply.github.com>
2024-10-21 16:50:51 +0800

    Update MyService.java

--- src/main/java/com/seg7/backendproject/MyService.java
+++ src/main/java/com/seg7/backendproject/MyService.java
@@ -1,1 +1,21 @@
+package com.seg7.backendproject;
+
+import org.springframework.stereotype.Service;
+import com.seg7.backendproject.Accounts;
+import com.seg7.backendproject.AccountRequest;
+
+@Service
+public class MyService {
+
+	public Accounts createProduct(AccountRequest request) {
+		Accounts accounts = new Accounts();
+		accounts.setRemark(request.getRemark());
+		accounts.setCategory(request.getCategory());
+		accounts.setAttach(request.getAttach());
+		accounts.setPrice(request.getPrice());
+
+		return accounts;
+	}
+
+}
 
