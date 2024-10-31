55d1e455bc6c9a05af10c56fd8da9b9b77d6cffa
lemonade-00 <68552165+lemonade-00@users.noreply.github.com>
2024-10-31 18:26:56 +0800

    Create QueryParameter.java

---
+++ src/main/java/com/seg7/backendproject/QueryParameter.java
@@ -0,0 +1,21 @@
+package com.seg7.backendproject;
+
+public class QueryParameter {
+
+    private String category;
+    private String startTime;
+    private String endTime;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+}
